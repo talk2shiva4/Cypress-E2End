71801a9f54603f53b342732d0a09df8aaa87935b
Shiv Anand <shiva9308@gmail.com>
2024-05-17 14:53:04 +0530

    bypass websecurity

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,15 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
   e2e: {
+    baseUrl:'https://qa-production.stealthplatform.cloud/loom',
+    chromeWebSecurity : false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   host:{
-    URL:'https://qa-production.stealthplatform.cloud/loom/purchase-order/23/row/add'
+    URL:'https://qa-production.stealthplatform.cloud/loom'
   }
 });
